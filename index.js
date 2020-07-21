c0e97693281619624c45908eae3bca5f5715cdda
Raymundo <aslan_ray@icloud.com>
2020-07-21 10:20:43 -0700

    App center analytics

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import { AppRegistry } from 'react-native';
-import Amplify, {Analytics} from 'aws-amplify';
-import config from './aws-exports';
 import App from './App.tsx';
 import { name as appName } from './app.json';
-
-Amplify.configure(config);
 
 AppRegistry.registerComponent(appName, () => App);
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -2,13 +2,11 @@ import React, { useLayoutEffect } from 'react';
 import { FlatList } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import Proptype from 'prop-types';
-import { Analytics, AWSKinesisProvider } from 'aws-amplify';
+import Analytics from 'appcenter-analytics';
 import { addToCart } from '../../redux/cart';
 import ProductItem from '../../components/shop/ProductItem.tsx';
 import CartHeaderButton from '../../components/ui/HeaderButton';
 import DrawerMenuButton from '../../components/ui/DrawerMenu';
-
-Analytics.addPluggable(new AWSKinesisProvider());
 
 const ProductOverviewScreen = ({ navigation }) => {
   const products = useSelector((state) => state.productReducer.availableProducts);
@@ -32,15 +30,7 @@ const ProductOverviewScreen = ({ navigation }) => {
             productTitle: itemData.item.title,
           })}
           onAddToCart={() => {
-            Analytics.record({
-              data: {
-                name: 'OBJECT ADDED TO CART',
-                attributes: {
-                  objectName: itemData.item.title,
-                },
-              },
-              streamName: 'AppShopDataStream',
-            }, 'AWSKinesis');
+            Analytics.trackEvent('ITEM ADDED TO CART', { itemName: itemData.item.title });
             dispatch(addToCart(itemData.item));
           }}
           isProductOverViewScreen
